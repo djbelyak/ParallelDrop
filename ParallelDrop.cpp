0634b13c4509d471f6ed878bbd584294f071be48
Ivan Belyavtsev <djbelyak@gmail.com>
2012-11-01 21:46:41 +0400

    Построение функции

--- ParallelDrop.cpp
+++ ParallelDrop.cpp
@@ -12,8 +12,13 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void
-init ()
+
+double f(double x, double y)
+{
+	return cos (pow(x,2)+pow(y,2));
+}
+
+void init ()
 {
   /*
    * Инициализация OpenGL
@@ -37,7 +42,7 @@ reshape (int w, int h)
   glMatrixMode (GL_PROJECTION);
   glLoadIdentity ();
   gluPerspective (45.0, (GLfloat)w/(GLfloat)h, 0.1, 1000.0);
-  gluLookAt(12.0, 12.0, 12.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  gluLookAt(12.0, 12.0, 12.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0);
   glMatrixMode (GL_MODELVIEW);
   glLoadIdentity ();
 
@@ -48,8 +53,7 @@ reshape (int w, int h)
  * Функция обратной связи GLUT при отрисовки окна.
  * Выполняет отрисовку OpenGL-сцены.
  */
-void
-display ()
+void display ()
 {
   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -69,6 +73,34 @@ display ()
   	  glColor3d(0.0, 0.0, 1.0);
       glVertex3d(0.0, 0.0, 0.0);
       glVertex3d(0.0, 0.0, 10.0);
+  glEnd();
+
+  long n = 1000; //Количество точек по x
+  long m = 1000; //Количество точек по y
+  double x_b=-10.0, x_e=10.0, y_b=-10.0, y_e=10.0;
+  double d_x = (x_e-x_b)/(double)n;
+  double d_y = (y_e-y_b)/(double)m;
+   glBegin(GL_TRIANGLES);
+  	  for (long i=0; i<n-1; i++)
+  		  for (long j=0; j<m-1;j++)
+  		  {
+  			  double x = x_b + i*d_x;
+  			  double y = y_b + j*d_y;
+
+  			  glColor3d (0.0, (1+f(x,y))/2.0, (1-f(x,y))/2.0);
+  			  glVertex3d (x, f(x,y), y);
+  			  glColor3d (0.0, (1+f(x+d_x,y))/2.0, (1-f(x+d_x,y))/2.0);
+  			  glVertex3d (x+d_x, f(x+d_x,y), y);
+  			  glColor3d (0.0, (1+f(x+d_x,y+d_y))/2.0, (1-f(x+d_x,y+d_y))/2.0);
+  			  glVertex3d (x+d_x, f(x+d_x, y+d_y), y+d_y);
+
+  			  glColor3d (0.0, (1+f(x,y))/2.0, (1-f(x,y))/2.0);
+  			  glVertex3d (x, f(x,y), y);
+  			  glColor3d (0.0, (1+f(x,y+d_y))/2.0, (1-f(x,y+d_y))/2.0);
+  			  glVertex3d (x, f(x,y+d_y), y+d_y);
+  			  glColor3d (0.0, (1+f(x+d_x,y+d_y))/2.0, (1-f(x+d_x,y+d_y))/2.0);
+  			  glVertex3d (x+d_x, f(x+d_x,y+d_y), y+d_y);
+  		  }
   glEnd();
 
 
