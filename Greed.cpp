c405061c7d5d86ff3e8b0e0b7ba1976484bb5823
Ivan Belyavtsev <djbelyak@gmail.com>
2012-11-09 12:12:32 +0400

    Написана половина каркаса класса Greed для построения полигональной сетки

---
+++ Greed.cpp
@@ -0,0 +1,22 @@
+/*
+ * Greed.cpp
+ *
+ *  Created on: 09.11.2012
+ *      Author: djbelyak
+ */
+
+#include "Greed.h"
+
+namespace CppParallelDrop {
+
+Greed::Greed(double b_x, double b_y, double e_x, double e_y, long m, long n)
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Greed::~Greed() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace CppParallelDrop */
---
+++ Greed.h
@@ -0,0 +1,63 @@
+/*
+ * Greed.h
+ *
+ *  Created on: 09.11.2012
+ *      Author: djbelyak
+ */
+
+#ifndef GREED_H_
+#define GREED_H_
+
+namespace CppParallelDrop
+{
+
+	class Greed
+	{
+	public:
+		//Конструктор
+		Greed(double, double, double, double, long, long);
+		//Метод получения X по итеративным координатам
+
+		//Виртуальный деструктор
+		virtual ~Greed();
+	private:
+		//Координата X начальной точки сетки
+		double BeginX;
+		//Координата Y начальной точки сетки
+		double BeginY;
+		//Координата X конечной точки сетки
+		double EndX;
+		//Координата Y конечной точки сетки
+		double EndY;
+		//Количество точек по оси X
+		long M;
+		//Количество точек по оси Y
+		long N;
+		//Функция расчета координаты по Z
+		static double CalcZ (double, double);
+		//Функция расчета красного цвета
+		static double CalcR (double);
+		//Функция расчета зеленого цвета
+		static double CalcG (double);
+		//Функция расчета синего цвета
+		static double CalcB (double);
+		//Метод построения сетки
+		static void Calculate ();
+		//Массив точек по X
+		double** X;
+		//Массив точек по Y
+		double** Y;
+		//Массив точек по Z
+		double** Z;
+		//Массив точек по R
+		double** R;
+		//Массив точек по G
+		double** G;
+		//Массив точек по B
+		double** B;
+
+	};
+
+}
+/* namespace CppParallelDrop */
+#endif /* GREED_H_ */
